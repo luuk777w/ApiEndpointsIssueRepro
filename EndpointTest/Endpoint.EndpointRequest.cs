38de802e4f517377179c79c6b183efc0dadc4f4a
Luuk Wuijster <luuk777w@users.noreply.github.com>
2022-01-17 23:34:43 +0200

    Init

---
+++ EndpointTest/Endpoint.EndpointRequest.cs
@@ -0,0 +1,16 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace EndpointTest
+{
+    public class EndpointRequest
+    {
+        [FromRoute(Name = "projectId")] public Guid ProjectId { get; set; } = default!;
+
+        [FromBody] public string Code { get; set; } = default!;
+
+        [FromBody] public string Description { get; set; } = default!;
+
+        [FromBody] public string Number { get; set; } = default!;
+    }
+}
---
+++ EndpointTest/Endpoint.cs
@@ -0,0 +1,21 @@
+﻿using Ardalis.ApiEndpoints;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EndpointTest
+{
+    public class Endpoint : EndpointBaseAsync
+                                        .WithRequest<EndpointRequest>
+                                        .WithActionResult<EndpointRequest>
+    {
+        public Endpoint()
+        {
+        }
+
+        [HttpPost("api/test/{projectId}")]
+        public override async Task<ActionResult<EndpointRequest>> HandleAsync([FromRoute] EndpointRequest request, CancellationToken cancellationToken = default)
+        {
+            return Ok(request);
+        }
+    }
+}
